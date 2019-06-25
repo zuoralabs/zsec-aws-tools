01a65118a9f11dab4e07ce63266161cdd9aff294
Andrew Lee <andrew-lee-zuora@users.noreply.github.com>
2019-06-25 14:06:29 -0700

    Add module `s3`

---
+++ testing/test_s3.py
@@ -0,0 +1,79 @@
+import pytest
+import enum
+import boto3
+import zsec_aws_tools.waf as zwaf
+from zsec_aws_tools.waf import RuleElement
+from zsec_aws_tools import scroll
+import toolz
+from zsec_aws_tools.basic import get_account_id
+import zsec_aws_tools.s3 as zaws_s3
+import uuid
+import random
+import string
+import logging
+
+
+@pytest.fixture
+def s3_bucket():
+    session = boto3.Session(profile_name='test')
+
+    account_id = get_account_id(session)
+
+    def bucket_policy(bucket_arn):
+        return {
+            "Version": "2012-10-17",
+            "Statement": [
+                {
+                    "Effect": "Allow",
+                    "Principal": {
+                        "AWS": account_id
+                    },
+                    "Action": [
+                        "s3:AbortMultipartUpload",
+                        "s3:GetBucketLocation",
+                        "s3:GetObject",
+                        "s3:ListBucket",
+                        "s3:ListBucketMultipartUploads",
+                        "s3:PutObject",
+                        "s3:PutObjectAcl"
+                    ],
+                    "Resource": [
+                        bucket_arn,
+                        "{bucket_arn}/*".format(bucket_arn=bucket_arn),
+                    ],
+                }
+            ]}
+
+    random_str = ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(10))
+
+    bucket_x = zaws_s3.Bucket(session=session, region_name='us-east-1',
+                              name='test-bucket-' + random_str,
+                              ztid=uuid.UUID('bf981b04-dae1-4b6c-a6c3-0d107eb4c847'),
+                              config=dict(ACL='private',
+                                          Policy=bucket_policy))
+
+    yield bucket_x
+
+    # don't care about consistency with bucket_x.exists; this is a fixture not a test
+    bucket_x.delete(not_exists_ok=True)
+
+
+def test_s3_bucket(s3_bucket, caplog):
+    caplog.set_level(logging.CRITICAL)
+
+    assert not s3_bucket.exists
+    s3_bucket.put()
+    assert s3_bucket.exists
+    assert s3_bucket._detect_existence_using_index_id()
+
+    s3_bucket.delete()
+    s3_bucket.wait_until_not_exists()
+    assert not s3_bucket.exists
+    assert not s3_bucket._detect_existence_using_index_id()
+
+    policy = s3_bucket.boto3_resource().Policy()
+    acl = s3_bucket.boto3_resource().Acl()
+
+    #print(acl.grants)
+    print(s3_bucket)
+    assert 1
---
+++ zsec_aws_tools/s3.py
@@ -0,0 +1,72 @@
+from typing import Optional, Dict
+import logging
+from .basic import (scroll, AWSResource, AwaitableAWSResource, manager_tag_key,
+                    add_manager_tags, add_ztid_tags)
+import botocore.exceptions
+import json
+
+logger = logging.getLogger(__name__)
+
+
+class Bucket(AwaitableAWSResource, AWSResource):
+    top_key = 'Bucket'
+    id_key = 'Bucket'
+    name_key = 'Bucket'
+    client_name = 's3'
+    sdk_name = 'bucket'
+    index_id_key = name_key
+    not_found_exception_name = 'NoSuchBucket'
+    existence_waiter_name = 'bucket_exists'
+    non_creation_parameters = ['Policy']
+
+    def _detect_existence_using_index_id(self) -> bool:
+        return self.boto3_resource().creation_date is not None
+
+    def _detect_existence_using_index_id_broken(self) -> bool:
+        """Broken implementation that uses recommended HeadBucket
+
+        HeadBucket is recommended by AWS to check existence, but it seems to be buggy.
+        This implementation is kept as a reference
+
+        """
+        try:
+            return self.index_id_key in self.service_client.head_bucket(Bucket=self.index_id)
+        except botocore.exceptions.ClientError as err:
+            # This seems to be a bug with IAM.
+            if err.response['Error'] == {'Code': '404', 'Message': 'Not Found'}:
+                raise
+
+    def _get_index_id_from_name(self) -> Optional[str]:
+        """Return ID using self.name
+
+        Requires that self.name is set and that it is unique.
+        Should only be called during `__init__` to set `self.id_`.
+
+        """
+        return self.name
+
+    def describe(self) -> Dict:
+        """
+        Do not call describe on a bucket. It doesn't do anything.
+        """
+        raise NotImplementedError
+
+    def boto3_resource(self):
+        return self.session.resource('s3').Bucket(self.name)
+
+    def put(self, wait: bool = True, force: bool = False):
+        if not self.exists:
+            logger.info('{} "{}" does not exist. Creating.'.format(self.top_key, self.name))
+            resp, _ = self.create(wait=wait)   # no index_id returned by CreateBucket.
+            self.exists = True
+
+        if 'Policy' in self.config:
+            policy = json.dumps(self.config['Policy'](self.arn))
+            self.boto3_resource().Policy().put(Policy=policy)
+
+    def wait_until_not_exists(self) -> None:
+        return self.boto3_resource().wait_until_not_exists()
+
+    @property
+    def arn(self):
+        return 'arn:aws:s3:::' + self.name
