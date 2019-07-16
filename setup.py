16ad0821148e2af6d194335f612a496d667bea27
Andrew Lee <andrew-lee-zuora@users.noreply.github.com>
2019-07-16 14:05:32 -0700

    Fix install_requires

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 setuptools.setup(
         name='zsec-aws-tools',
         packages=['zsec_aws_tools'],
-        install_requires=['boto3'],
+        install_requires=['boto3', 'toolz'],
         tests_require=['toolz', 'pytest'],
         version='0.1.3',
         )
