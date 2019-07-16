9be96c502efe1658b9feaad8c54fdb7a1b721ea8
Andrew Lee <andrew-lee-zuora@users.noreply.github.com>
2019-07-16 14:02:41 -0700

    Put version in setup.py

--- setup.py
+++ setup.py
@@ -4,5 +4,6 @@ setuptools.setup(
         name='zsec-aws-tools',
         packages=['zsec_aws_tools'],
         install_requires=['boto3'],
-        tests_require=['toolz', 'pytest']
+        tests_require=['toolz', 'pytest'],
+        version='0.1.3',
         )
