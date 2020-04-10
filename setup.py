beed00071c1edfc9a17b19be3687aa7dfe913114
Andrew Lee <andrew-lee-zuora@users.noreply.github.com>
2020-04-10 13:08:18 -0700

    Add attrs to deps

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 setuptools.setup(
         name='zsec-aws-tools',
         packages=['zsec_aws_tools'],
-        install_requires=['boto3', 'toolz', 'pynamodb'],
+        install_requires=['boto3', 'toolz', 'pynamodb', 'attrs'],
         tests_require=['toolz', 'pytest'],
         version='0.1.20',
         )
