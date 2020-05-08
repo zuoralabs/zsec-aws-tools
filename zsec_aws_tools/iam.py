42a7ad8fb4ecf577e8e3c611b4a9617b427a1b90
Andrew Lee <andrew-lee-zuora@users.noreply.github.com>
2020-05-07 21:23:55 -0700

    Add `session_with_aliasing_workaround`

--- zsec_aws_tools/iam.py
+++ zsec_aws_tools/iam.py
@@ -1,5 +1,7 @@
 import json
 import logging
+from pathlib import Path
+
 import boto3
 import uuid
 from functools import partialmethod, partial
@@ -24,6 +26,52 @@ aws_access_key_id = {aws_access_key_id}
 aws_secret_access_key = {aws_secret_access_key}
 aws_session_token = {aws_session_token}
 """
+
+
+def session_with_aliasing_workaround(profile_name=None, region_name=None):
+    """Wrapper around boto3.Session that behaves better with `~/.aws/config`
+
+    Will follow the source_profile of `profile_name` in `~/.aws/config` if found
+    and the only keys it has are `source_profile` and `region`. More complicated
+    configurations are not supported for the workaround behavior,
+    and this function reverts to the original behavior of `boto3.Session`.
+
+    If `profile_name` is not found in `~/.aws/config`, this function reverts to
+    the original behavior of `boto3.Session`.
+
+    Note that this function is necessary because `boto3.Session` will
+    ignore aliases for some complicated configurations of `source_profile`.
+
+    """
+
+    import configparser
+
+    config = configparser.ConfigParser()
+    config.read(Path('~/.aws/config').expanduser())
+
+    cnt = 0
+    while cnt < 5:
+        cnt += 1
+        if profile_name:
+            try:
+                profile_config = config['profile ' + profile_name]
+            except KeyError:
+                break
+            else:
+                if set(profile_config.keys()) <= {'source_profile', 'region'}:
+                    new_profile_name = profile_config.get('source_profile', profile_name)
+                    region_name = profile_config.get('region', region_name)
+                    if new_profile_name == profile_name:
+                        break
+                    else:
+                        profile_name = new_profile_name
+                else:
+                    break
+
+        else:
+            break
+
+    return boto3.Session(profile_name=profile_name, region_name=region_name)
 
 
 def assume_role_response_to_session_kwargs(assume_role_resp):
