0c52fc26fbd7b6b64bb39085394126aca487e6a4
Andrew Lee <andrew-lee-zuora@users.noreply.github.com>
2020-05-18 17:49:20 -0700

    Implement `resolve_profile_alias`

--- zsec_aws_tools/iam.py
+++ zsec_aws_tools/iam.py
@@ -28,6 +28,53 @@ aws_session_token = {aws_session_token}
 """
 
 
+def resolve_profile_alias(profile_name=None, region_name=None):
+    """Wrapper around boto3.Session that behaves better with `~/.aws/config`
+
+    Will follow the source_profile of `profile_name` in `~/.aws/config` if found
+    and the only keys it has are `source_profile` and `region`. More complicated
+    configurations are not supported for the workaround behavior,
+    and this function reverts to the original behavior of `boto3.Session`.
+
+    If `profile_name` is not found in `~/.aws/config`, this function reverts to
+    the original behavior of `boto3.Session`.
+
+    Note that this function is necessary because `boto3.Session` will
+    ignore aliases for some complicated configurations of `source_profile`.
+
+    """
+
+    import configparser
+
+    config = configparser.ConfigParser()
+    config.read(Path('~/.aws/config').expanduser())
+
+    cnt = 0
+    while cnt < 5:
+        cnt += 1
+        if profile_name:
+            try:
+                profile_config = config['profile ' + profile_name]
+            except KeyError:
+                break
+            else:
+                if set(profile_config.keys()) <= {'source_profile', 'region'}:
+                    new_profile_name = profile_config.get('source_profile', profile_name)
+                    if region_name is None:
+                        region_name = profile_config.get('region', region_name)
+                    if new_profile_name == profile_name:
+                        break
+                    else:
+                        profile_name = new_profile_name
+                else:
+                    break
+
+        else:
+            break
+
+    return profile_name, region_name
+
+
 def session_with_aliasing_workaround(profile_name=None, region_name=None):
     """Wrapper around boto3.Session that behaves better with `~/.aws/config`
 
